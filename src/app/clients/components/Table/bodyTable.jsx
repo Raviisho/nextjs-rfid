ab699c33adb18b9d1645794f400fd3811374508f
Raviisho <marcoslescano02@gmail.com>
2023-10-27 23:36:49 -0300

    table-Clientes

--- src/app/clients/components/Table/bodyTable.jsx
+++ src/app/clients/components/Table/bodyTable.jsx
@@ -9,18 +9,6 @@ export default function TableBody({ client }) {
   const pathname = usePathname();
   return (
     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-      <td className="w-4 p-4">
-        <div className="flex items-center">
-          <input
-            id="checkbox-table-search-1"
-            type="checkbox"
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label htmlFor="checkbox-table-search-1" className="sr-only">
-            checkbox
-          </label>
-        </div>
-      </td>
       <th
         scope="row"
         className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
