1be1f94c539063a6b03a22cba1bdc79ecbef6d33
Marcos Lescano <marcoslescano02@gmail.com.com>
2023-11-03 10:13:46 -0300

    .

--- src/app/clients/libs/getClients.jsx
+++ src/app/clients/libs/getClients.jsx
@@ -1,5 +1,5 @@
 export default async function getClients() {
-  const response = await fetch("http://127.0.0.1:3000/api/clients");
+  const response = await fetch("/api/clients");
 
   if (!response.ok) {
     throw new Error("failed to fetch clients");
