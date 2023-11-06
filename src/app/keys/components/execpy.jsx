1e39ddc16482bef98acaaa29a1c2ef9d9873b276
Marcos Lescano <marcoslescano02@gmail.com.com>
2023-11-06 18:07:36 -0300

    ..0

---
+++ src/app/keys/components/execpy.jsx
@@ -0,0 +1,15 @@
+import { exec } from 'child_process';
+
+export default function pruebaSubmit() {
+
+    // Comando para ejecutar un script de Python
+    const comando = "python3 src/assets/python/prueba.py";
+
+    exec(comando, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error}`);
+        return;
+      }
+      console.log(`Resultado: ${stdout}`);
+    });
+}
--- src/app/keys/page.jsx
+++ src/app/keys/page.jsx
@@ -1,24 +1,10 @@
-"use client";
-import { exec } from 'child_process';
+import pruebaSubmit from "@/app/keys/components/execpy.jsx"
 export default function KeysPage() {
-  function pruebaSubmit() {
-
-    // Comando para ejecutar un script de Python
-    const comando = "python3 src/assets/python/prueba.py";
-
-    exec(comando, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error: ${error}`);
-        return;
-      }
-      console.log(`Resultado: ${stdout}`);
-    });
-  }
-
+  pruebaSubmit()
   return (
     <>
       <div>Keys Works</div>
-      <button onClick={pruebaSubmit()}>TEST</button>
+      <button>TEST</button>
     </>
   );
 }
