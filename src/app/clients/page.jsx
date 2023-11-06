bb7021e3a59e74a68f1d1f1963475354255ba965
Marcos Lescano <marcoslescano02@gmail.com.com>
2023-11-06 10:51:41 -0300

    .

--- src/app/clients/page.jsx
+++ src/app/clients/page.jsx
@@ -1,19 +1,28 @@
-import { Suspense } from "react";
-import getClients from "./libs/getClients";
+// Importa las bibliotecas necesarias
 import SearchBar from "./components/searchbar";
 import Table from "./components/Table/table";
 
-export default async function ClientesPage() {
-  const clients = await getClients()
-
+// Define la función de la página (antes de cualquier exportación)
+function ClientesPage({ clients }) {
   return (
     <>
       <div className="m-5 overflow-x-auto shadow-md sm:rounded-lg">
         <SearchBar />
-        <Suspense fallback={<div>Cargando clientes...</div>}>
-          <Table clients={clients} />
-        </Suspense>
+        <Table clients={clients} />
       </div>
     </>
   );
+}
+
+// Exporta la función de la página
+export default ClientesPage;
+
+// Incorpora la función getServerSideProps para recuperar los datos
+export async function getServerSideProps() {
+  // Aquí recuperas los datos antes de la renderización de la página
+  const clients = await getClients();
+
+  return {
+    props: { clients },
+  };
 }
