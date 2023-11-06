981434c4699801128f7d19c1b9b15fb9f5602de8
Marcos Lescano <marcoslescano02@gmail.com.com>
2023-11-06 11:01:03 -0300

    .

--- src/app/clients/page.jsx
+++ src/app/clients/page.jsx
@@ -1,8 +1,7 @@
-// Importa las bibliotecas necesarias
-import SearchBar from "./components/searchbar";
-import Table from "./components/Table/table";
+import SearchBar from "../../../components/searchbar";
+import Table from "../../../components/Table/table";
+import getClients from "../../../libs/getClients";
 
-// Define la función de la página (antes de cualquier exportación)
 function ClientesPage({ clients }) {
   return (
     <>
@@ -14,15 +13,12 @@ function ClientesPage({ clients }) {
   );
 }
 
-// Exporta la función de la página
-export default ClientesPage;
-
-// Incorpora la función getServerSideProps para recuperar los datos
 export async function getServerSideProps() {
-  // Aquí recuperas los datos antes de la renderización de la página
   const clients = await getClients();
 
   return {
     props: { clients },
   };
 }
+
+export default ClientesPage;
